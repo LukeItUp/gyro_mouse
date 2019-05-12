bf87c8c9c893fb5bc2e55d550e9fafb64efc3f77
Luka Kavcic <luka.kavcic95@gmail.com>
2019-05-12 15:36:27 +0200

    Add files via upload

--- mouse.py
+++ mouse.py
@@ -22,11 +22,15 @@ def moveMouseTo(mouse, Newpos):
 	return
 
 
+# ---------------------------------------------
 ser = serial.Serial("/dev/ttyUSB0", 115200)
 mouse = Controller()
-
+button = [False, False, None ,False]
+counterBTN4 = 0
 pos = getScreenResolution()
 pos = [ pos[0]/2, pos[1]/2 ]
+mouse.release(Button.left)
+mouse.release(Button.right)
 
 while True:
 	# Reset values
@@ -36,29 +40,44 @@ while True:
 
 	# Read new values
 	line = ser.readline().split("\r\n")[0]
-	if (line == "BTN1: 1"):
+	if ((line == "BTN1: 1") and (not button[0])):
 		mouse.press(Button.right)
-		print("Pressed Right Mouse Button")
-	else:
+		button[0] = True
+
+	elif ((line == "BTN1: 0") and button[0]):
+		button[0] = False
 		mouse.release(Button.right)
 
-	if (line == "BTN2: 1"):
+	if ((line == "BTN2: 1") and (not button[1])):
 		mouse.press(Button.left)
-                print("Pressed Left Mouse Button")
-	else:
+		button[1] = True
+
+	elif ((line == "BTN2: 0") and button[1]):
 		mouse.release(Button.left)
+		button[1] = False
+
+	if (line == "BTN4: 1"):
+		button[3] = True
+		counterBTN4 += 1
+		if counterBTN4 > 15:
+			pos = getScreenResolution()
+			pos = [pos[0]/2, pos[1]/2]
+			counterBTN4 = 0
+	elif (line == "BTN4: 0"):
+		button[3] = False
 
 	if line.split(":")[0] == "GX":
-		gx = -int(float(line.split(" ")[1]))
+		gx = -float(line.split(" ")[1])
 
 	if line.split(":")[0] == "GY":
-		gy = int(float(line.split(" ")[1]))
+		gy = -float(line.split(" ")[1])
 
 	if line.split(":")[0] == "scroll_d":
 		scroll = int(float(line.split(" ")[1]))
 
+	if not button[3] :
+		pos[0] = pos[0] + gx
+		pos[1] = pos[1] + gy
 
-	pos[0] = pos[0] + gx
-	pos[1] = pos[1] + gy
 	moveMouseTo(mouse, pos)
 	mouse.scroll(0, scroll/10)
